0ca6770bd6c3327da755f8ae8eb7d19befd89239
Marcos <marcosantoniozovico@hotmail.com>
2017-03-05 18:25:25 -0300

    MaterialDrawer: Explorando opções adicionais

--- app/src/main/java/app/devmedia/com/br/appdevmedia/MainActivity.java
+++ app/src/main/java/app/devmedia/com/br/appdevmedia/MainActivity.java
@@ -1,5 +1,6 @@
 package app.devmedia.com.br.appdevmedia;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -9,12 +10,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.holder.BadgeStyle;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import app.devmedia.com.br.appdevmedia.adapter.ViewPagerAdapter;
 import app.devmedia.com.br.appdevmedia.fragment.FragmentPerfil;
@@ -45,10 +51,28 @@ public class MainActivity extends AppCompatActivity {
 
 
         PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Produtos");
-        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("Perfil");
+        item1.withSelectable(false);
+        final SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("Perfil");
 
-//create the drawer and remember the `Drawer` result object
+
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Marcos Souza").withEmail("msouza@gmail.com").withIcon(R.drawable.profile)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        return false;
+                    }
+
+                })
+                .build();
+
+
         drawer = new DrawerBuilder()
+                .withAccountHeader(headerResult)
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .addDrawerItems(
@@ -59,15 +83,32 @@ public class MainActivity extends AppCompatActivity {
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        viewPager.setCurrentItem(position);
-                        drawer.closeDrawer();
+                        prepararDrawerItem(position, item2);
                         return  true;
                     }
                 })
                 .build();
 
+    }
 
+    private void prepararDrawerItem(int position, SecondaryDrawerItem item2) {
+        viewPager.setCurrentItem(position);
+        drawer.closeDrawer();
 
+        switch (position){
+            case 0:
+                item2.withName("Segundo Texto")
+                        .withBadge("Novo")
+                        .withBadgeStyle(new BadgeStyle().withTextColor(Color.WHITE).withColorRes(R.color.md_orange_700));
+
+                drawer.updateItem(item2);
+                break;
+
+            case  2:
+                drawer.addItem(new DividerDrawerItem());
+                drawer.addStickyFooterItem(new PrimaryDrawerItem().withName("StickFooterItem"));
+                break;
+        }
     }
 
 
