c852c9132e76b961dbb595577b966877c7d8ed37
Marcos <marcosantoniozovico@hotmail.com>
2017-04-23 20:19:02 -0300

    Finalizando utilitário de notificações

--- app/src/main/java/app/devmedia/com/br/appdevmedia/notification/NotificationUtil.java
+++ app/src/main/java/app/devmedia/com/br/appdevmedia/notification/NotificationUtil.java
@@ -3,15 +3,27 @@ package app.devmedia.com.br.appdevmedia.notification;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.Patterns;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import app.devmedia.com.br.appdevmedia.R;
 
 /**
  * Created by Diogo on 15/05/2016.
@@ -19,11 +31,46 @@ import java.util.Date;
 public class NotificationUtil {
 
     public static final int NOTIFICATION_ID = 357;
+    public static final int BIG_NOTIFICATION_ID = 358;
 
     private Context context;
 
     public NotificationUtil(Context context) {
         this.context = context;
+    }
+
+
+    public void showSmallNotificationMsg(String titulo, String msg, String timestamp, Intent intent) {
+        showBigNotificationMsg(titulo, msg, timestamp, intent, null);
+    }
+
+    public void showBigNotificationMsg(String titulo, String msg, String timestamp, Intent intent, String imgUrl) {
+
+        if (TextUtils.isEmpty(msg)) {
+            return;
+        }
+
+        final int icon = R.mipmap.ic_launcher;
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        Uri somAlarme = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/raw/notification");
+
+        if (TextUtils.isEmpty(imgUrl)) {
+            showSmallNotification(builder, icon, titulo, msg, timestamp, pendingIntent, somAlarme);
+        } else {
+            if (imgUrl.length() > 4 && Patterns.WEB_URL.matcher(imgUrl).matches()) {
+                Bitmap bitmap = getBitmapFromURL(imgUrl);
+                if (bitmap != null)
+                    showBigNotification(bitmap, builder, icon, titulo, msg, timestamp, pendingIntent, somAlarme);
+                else
+                    showSmallNotification(builder, icon, titulo, msg, timestamp, pendingIntent, somAlarme);
+            }
+        }
+
     }
 
     private void showSmallNotification(NotificationCompat.Builder builder, //
@@ -62,6 +109,26 @@ public class NotificationUtil {
                                      String timestamp, //
                                      PendingIntent pendingIntent, //
                                      Uri somAlarme) {
+        NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+        bigPictureStyle.setBigContentTitle(titulo);
+        bigPictureStyle.setSummaryText(Html.fromHtml(msg).toString());
+        bigPictureStyle.bigPicture(bitmap);
+
+        Notification notification = builder.setSmallIcon(icon)
+                .setTicker(titulo)
+                .setContentTitle(titulo)
+                .setContentText(msg)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setSound(somAlarme)
+                .setWhen(getTimeInMilliSec(timestamp))
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), icon))
+                .setNumber(4)
+                .setStyle(bigPictureStyle)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(BIG_NOTIFICATION_ID, notification);
 
     }
 
@@ -75,6 +142,22 @@ public class NotificationUtil {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    public Bitmap getBitmapFromURL(String stringUrl) {
+        try {
+            URL url = new URL(stringUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            return BitmapFactory.decodeStream(inputStream);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
