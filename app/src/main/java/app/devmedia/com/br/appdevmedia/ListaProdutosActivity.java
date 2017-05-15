fc287db4c71718d686f01c74918fb5f0987a13e4
Marcos <marcosantoniozovico@hotmail.com>
2017-05-14 22:15:41 -0300

    Criando Activity de listagem

--- app/src/main/java/app/devmedia/com/br/appdevmedia/ListaProdutosActivity.java
+++ app/src/main/java/app/devmedia/com/br/appdevmedia/ListaProdutosActivity.java
@@ -1,15 +1,67 @@
 package app.devmedia.com.br.appdevmedia;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class ListaProdutosActivity extends AppCompatActivity {
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalService;
+
+import java.util.List;
+
+import app.devmedia.com.br.appdevmedia.adapter.CustomProdutoAdapter;
+import app.devmedia.com.br.appdevmedia.entity.Produto;
+import app.devmedia.com.br.appdevmedia.util.Constantes;
+
+public class ListaProdutosActivity extends AppCompatActivity implements CustomProdutoAdapter.ProdutoListAdapterListener {
+
+    private ListView lstProdutos;
+    private Button btnCheckout;
+
+    private CustomProdutoAdapter customProdutoAdapter;
+    private List<Produto> produtos;
+
+    private ProgressDialog progressDialog;
+
+    private PayPalConfiguration payPalConfig = new PayPalConfiguration().environment(Constantes.PAYPAL_ENV)
+                                                                        .clientId(Constantes.PAYPAL_CLIENT_ID)
+                                                                        .languageOrLocale("pt_BR");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista_produtos);
 
+        lstProdutos = (ListView) findViewById(R.id.lstProdutos);
+        btnCheckout = (Button) findViewById(R.id.btnCheckout);
+
+        customProdutoAdapter = new CustomProdutoAdapter(this, produtos, this);
+        lstProdutos.setAdapter(customProdutoAdapter);
+
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, payPalConfig);
+        startService(intent);
+
+        buscarListaProdutos();
+    }
+
+    private void buscarListaProdutos() {
+        showProgressDialog();
+
+    }
+
+    @Override
+    public void onAddCarrinhoPressed(Produto produto) {
+
+    }
+
+    private void showProgressDialog() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Carregando...");
+        progressDialog.show();
     }
 }
--- app/src/main/java/app/devmedia/com/br/appdevmedia/adapter/CustomProdutoAdapter.java
+++ app/src/main/java/app/devmedia/com/br/appdevmedia/adapter/CustomProdutoAdapter.java
@@ -1,12 +1,23 @@
 package app.devmedia.com.br.appdevmedia.adapter;
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
+import app.devmedia.com.br.appdevmedia.DevMediaApp;
+import app.devmedia.com.br.appdevmedia.R;
 import app.devmedia.com.br.appdevmedia.entity.Produto;
 
 /**
@@ -18,6 +29,9 @@ public class CustomProdutoAdapter extends BaseAdapter {
     private final List<Produto> produtos;
     private final ProdutoListAdapterListener produtoListAdapterListener;
 
+    private LayoutInflater inflater;
+    private ImageLoader imageLoader = DevMediaApp.getInstance().getImageLoader();
+
     public CustomProdutoAdapter( final Activity activity, final List<Produto> produtos, final ProdutoListAdapterListener produtoListAdapterListener) {
         this.activity = activity;
         this.produtos = produtos;
@@ -26,22 +40,56 @@ public class CustomProdutoAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return produtos.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return produtos.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if (inflater == null) {
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.linha_produto2, null);
+        }
+
+        NetworkImageView imgProduto = (NetworkImageView) convertView.findViewById(R.id.imgProduto);
+        TextView txtTitulo = (TextView) convertView.findViewById(R.id.txtTitulo);
+        TextView txtDescricao = (TextView) convertView.findViewById(R.id.txtDescricao);
+        TextView txtPreco = (TextView) convertView.findViewById(R.id.txtPreco);
+        Button btnAddCarrinho = (Button) convertView.findViewById(R.id.btnAddCarrinho);
+
+        final Produto produto = produtos.get(position);
+
+        imgProduto.setImageUrl(produto.getUrlImg(), imageLoader);
+
+        txtTitulo.setText(produto.getTitulo());
+        txtDescricao.setText(produto.getDescricao());
+
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(new Locale("pt"));
+        String valor = "R$ " + numberFormat.format(produto.getValor());
+
+        txtPreco.setText(valor);
+
+        btnAddCarrinho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                produtoListAdapterListener.onAddCarrinhoPressed(produto);
+            }
+        });
+
+        return convertView;
     }
 
     public interface ProdutoListAdapterListener {
